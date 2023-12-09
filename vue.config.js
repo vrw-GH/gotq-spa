4317f1ef3187ce4b098af203028907b5703a22be
Victor Wright <victor.wright@outlook.de>
2023-12-09 18:54:38 +0100

    docs folder and publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/",
+  devServer: {
+    port: 3000,
+  },
 });
